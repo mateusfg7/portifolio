ef8fce468fd74f767806e863a569445dea5831d8
mateusfg7 <mateusfg77@gmail.com>
2019-11-16 09:01:26 -0300

    Updates

---
+++ precache-manifest.18f9cd65ff9429fd690b07a2a4131598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1656a0e5f0100133996fa89fbdc7b6f3",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "c40cb7c870f1ee17c0d0",
+    "url": "/portifolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b5fb911ed79fd064c85b",
+    "url": "/portifolio/static/js/2.0bc79280.chunk.js"
+  },
+  {
+    "revision": "c40cb7c870f1ee17c0d0",
+    "url": "/portifolio/static/js/main.6191799b.chunk.js"
+  },
+  {
+    "revision": "58a82909529b9bd68a83",
+    "url": "/portifolio/static/js/runtime-main.fb6e1c4d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portifolio/static/media/logo.25bf045c.svg"
+  }
+]);
