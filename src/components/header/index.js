2582c043bc7cc52629f4167aa42ed00184741316
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2019-11-27 18:39:39 +0000

    update header and body style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './style.css';
+import './effect.css';
 
 const Header = () => (
     <header id="main-header">
-        <h1>Hi! I'm Mateus Felipe.</h1>
-        <h1></h1>
+        <div className="center title">
+            <h1>Hi! I'm Mateus Felipe.</h1>
+        </div>
+        <div className="center link">
+            <div>
+                <h1>⇣</h1>
+            </div>
+            
+        </div>
     </header>
 );
 
