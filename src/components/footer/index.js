c57f8e761d4d73585dac86f530c591766566e68b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2019-11-27 18:56:38 +0000

    add social links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => (
     <footer>
         <h1 className="titulo">Redes Sociais</h1>
         <p>
-            <a href="#" target="_blank">Facebook</a> • <a href="#" target="_blank">Twitter</a> • <a href="#" target="_blank">Instagram</a> • <a href="#" target="_blank">Linkedin</a>
+            <a href="https://www.facebook.com/mateusfelipefg7" target="_blank">Facebook</a> • <a href="https://twitter.com/mateusfg77" target="_blank">Twitter</a> • <a href="https://www.instagram.com/mateusfg7/" target="_blank">Instagram</a> • <a href="https://www.linkedin.com/in/mateus-felipe-a90030152/" target="_blank">Linkedin</a>
         </p>
         <h2 className="copy">&copy; Mateus Felipe Gonçalves</h2>
     </footer>
