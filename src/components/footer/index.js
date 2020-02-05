51e36e8902c55e87f027fec6f757e16b3f91169d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-02-05 15:09:38 +0000

    update footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-import './style.css';
+import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+
+import icon from '../../services/fontawesome'
+/* import './style.css'; */
+import './fix-style.css'
 
 const Footer = () => (
-    <footer>
-        <h1 className="titulo">Redes Sociais</h1>
-        <p>
-            <a href="https://www.facebook.com/mateusfelipefg7" target="_blank">Facebook</a> • <a href="https://twitter.com/mateusfg77" target="_blank">Twitter</a> • <a href="https://www.instagram.com/mateusfg7/" target="_blank">Instagram</a> • <a href="https://www.linkedin.com/in/mateus-felipe-a90030152/" target="_blank">Linkedin</a>
-        </p>
-        <h2 className="copy">&copy; Mateus Felipe Gonçalves</h2>
+    <footer className="container-fluid pt-3 footer text-light bg-dark text-center">
+        
+        
+        {/* <h1 className="titulo">Redes Sociais</h1> */}
+        
+        <container className="container col-6 col-lg-4 justify-content-around d-flex">
+        <a href="https://www.facebook.com/mateusfelipefg7" target="_blank">{icon(faFacebookF)}</a>
+        •
+        <a href="https://twitter.com/mateusfg77" target="_blank">{icon(faTwitter)}</a>
+        •
+        <a href="https://www.instagram.com/mateusfg7/" target="_blank">{icon(faInstagram)}</a>
+        •
+        <a href="https://www.linkedin.com/in/mateus-felipe-a90030152/" target="_blank">{icon(faLinkedinIn)}</a>
+        </container>
+        
+        {/* <p className="copy">&copy; Mateus Felipe Gonçalves</p> */}
     </footer>
 );
 
