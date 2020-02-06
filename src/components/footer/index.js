512517ce3b59f0bef3b450e90496bf61ccbacfc1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-02-06 13:49:16 +0000

    footer complete

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,26 +4,25 @@ import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
 
 import icon from '../../services/fontawesome'
-/* import './style.css'; */
-import './fix-style.css'
+
+import './style.css';
+import './style-hover.css'
 
 const Footer = () => (
     <footer className="container-fluid pt-3 footer text-light bg-dark text-center">
-        
-        
-        {/* <h1 className="titulo">Redes Sociais</h1> */}
-        
-        <container className="container col-6 col-lg-4 justify-content-around d-flex">
-        <a href="https://www.facebook.com/mateusfelipefg7" target="_blank">{icon(faFacebookF)}</a>
+            
+        <h2 className="titulo">Redes Sociais</h2>
+        <container className="container col-6 col-lg-4 bg-dark justify-content-around d-flex">
+        <a href="https://www.facebook.com/mateusfelipefg7" target="_blank" className="face">{icon(faFacebookF)}</a>
         •
-        <a href="https://twitter.com/mateusfg77" target="_blank">{icon(faTwitter)}</a>
+        <a href="https://twitter.com/mateusfg77" target="_blank" className="tt">{icon(faTwitter)}</a>
         •
-        <a href="https://www.instagram.com/mateusfg7/" target="_blank">{icon(faInstagram)}</a>
+        <a href="https://www.instagram.com/mateusfg7/" target="_blank" className="insta">{icon(faInstagram)}</a>
         •
-        <a href="https://www.linkedin.com/in/mateus-felipe-a90030152/" target="_blank">{icon(faLinkedinIn)}</a>
+        <a href="https://www.linkedin.com/in/mateus-felipe-a90030152/" target="_blank" className="in">{icon(faLinkedinIn)}</a>
         </container>
         
-        {/* <p className="copy">&copy; Mateus Felipe Gonçalves</p> */}
+        <p className="copy">&copy; Mateus Felipe Gonçalves</p>
     </footer>
 );
 
