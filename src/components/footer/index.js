6a6c4b369204a83cedcf4930ef3217e2ede4a99a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-02-17 19:47:06 +0000

    index.js footer updates

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,20 +9,19 @@ import './style.css'
 import './style-hover.css'
 
 const Footer = () => (
-    <footer className="container-fluid pt-3 footer text-light bg-dark text-center">
-            
-        <h2 className="titulo">Redes Sociais</h2>
-        <section className="container-fluid col-6 col-lg-4 bg-dark justify-content-around d-flex">
-        <a href="https://www.facebook.com/mateusfelipefg7" target="_blank" className="face">{icon(faFacebookF)}</a>
-        •
-        <a href="https://twitter.com/mateusfg77" target="_blank" className="tt">{icon(faTwitter)}</a>
-        •
-        <a href="https://www.instagram.com/mateusfg7/" target="_blank" className="insta">{icon(faInstagram)}</a>
-        •
-        <a href="https://www.linkedin.com/in/mateus-felipe-a90030152/" target="_blank" className="in">{icon(faLinkedinIn)}</a>
-        </section>
+    <footer className="container-fluid pt-3 footer bg-dark text-light text-center">
         
-        <p className="copy">&copy; Mateus Felipe Gonçalves</p>
+        <h2 className="titulo">Redes Sociais</h2>
+        <div className="container col-6 col-md-4 d-flex justify-content-around">
+            <a href="https://www.facebook.com/mateusfelipefg7" target="_blank" className="face">{icon(faFacebookF)}</a>
+            •
+            <a href="https://twitter.com/mateusfg77" target="_blank" className="tt">{icon(faTwitter)}</a>
+            •
+            <a href="https://www.instagram.com/mateusfg7/" target="_blank" className="insta">{icon(faInstagram)}</a>
+            •
+            <a href="https://www.linkedin.com/in/mateus-felipe-a90030152/" target="_blank" className="in">{icon(faLinkedinIn)}</a>
+        </div>
+        <span className="copy bg-dark">&copy; Mateus Felipe Gonçalves</span>
     </footer>
 );
 
