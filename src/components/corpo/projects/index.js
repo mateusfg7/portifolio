49de46b824321812be6822f6bd6ef23fa4798d21
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-02-19 17:50:48 +0000

    fix github api rate limit bug

--- src/components/corpo/projects/index.js
+++ src/components/corpo/projects/index.js
@@ -9,23 +9,28 @@ export default function Imagem() {
     useEffect(async () => {
         const response = await fetch('https://api.github.com/users/mateusfg7/repos')
         const data = await response.json()
-        setRepositories([data[10], data[27], data[13], data[3], data[9], data[28]])
         
+        if (response.status == 403) {
+            setRepositories(data)
+        } 
+        else if (response.status == 200) {
+            console.log(response.status)
+            setRepositories([data[10], data[27], data[13], data[3], data[9], data[28]])
+        }      
     }, [])
 
     return(
         <div className="main">
         
             <h2 id="projetos">Projetos</h2>
-            {console.log(repositories)}
-            { repositories.map(repo => (
+            {repositories.map(repo => (
                 <div className="project">
                     <h3>{repo.name}</h3>
                     <p>{repo.description}</p>
                     <p>{repo.language}</p>
                     <a href={repo.svn_url} target="_blank">GitHub</a>
                 </div>
-            )) }
+            ))}
         </div>
     )
 }
