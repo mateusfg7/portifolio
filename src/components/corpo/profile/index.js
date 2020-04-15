db6071672211606c30ed6bfda93aed4bf11c6539
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-15 12:00:53 -0300

    clean code

--- src/components/corpo/profile/index.js
+++ src/components/corpo/profile/index.js
@@ -1,32 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './style.css';
+import "./style.css";
 
-import whatsapp from './img/whatsapp.png';
-import gmail from './img/gmail.png';
+import whatsapp from "./img/whatsapp.png";
+import gmail from "./img/gmail.png";
 
-import api from '../../../services/api';
+import api from "../../../services/api";
 
 const MainSkills = () => {
-    const [profile, setProfile] = useState([]);
+  const [profile, setProfile] = useState([]);
 
-    useEffect(async () => {
-        const response = await api.get('/users/mateusfg7');
-        setProfile({ avatar: response.data.avatar_url });
-    }, []);
+  useEffect(async () => {
+    const response = await api.get("/users/mateusfg7");
+    setProfile({ avatar: response.data.avatar_url });
+  }, []);
 
-    return (
-      <ul className="list-unstyled main-section">
-        <li className="media">
-          <img src={profile.avatar} className="mr-3 profile" />
-          <div className="media-body">
-            <h5 className="mt-0 mb-1" id="skills">My Skills</h5>
-            eu sou eu, sendo eu, porque se eu não for eu quem sera eu, nesse caso eu sou obrigado a ser eu!
-            (texto teste)
-          </div>
-        </li>
-      </ul>
-    );
+  return (
+    <ul className="list-unstyled main-section">
+      <li className="media">
+        <img src={profile.avatar} className="mr-3 profile" />
+        <div className="media-body">
+          <h5 className="mt-0 mb-1" id="skills">
+            My Skills
+          </h5>
+          eu sou eu, sendo eu, porque se eu não for eu quem sera eu, nesse caso
+          eu sou obrigado a ser eu! (texto teste)
+        </div>
+      </li>
+    </ul>
+  );
 };
 
 export default MainSkills;
