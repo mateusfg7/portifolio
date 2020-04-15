b4c1169cd553dca55bb6e637c8c1560cd2e540ee
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-15 11:52:30 -0300

    Fix issue #10: 'The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value.'

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import './style.css';
+import "./style.css";
 
-import { faCoffee, faFolderMinus } from '@fortawesome/free-solid-svg-icons';
-import icon from '../../services/fontawesome';
+import { faCoffee, faFolderMinus } from "@fortawesome/free-solid-svg-icons";
+import icon from "../../services/fontawesome";
 
 const Header = () => (
   <header>
@@ -12,7 +12,6 @@ const Header = () => (
       <span id="piscar">|</span>
     </h1>
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-
       <button
         className="navbar-toggler"
         type="button"
@@ -25,21 +24,26 @@ const Header = () => (
         <span className="navbar-toggler-icon" />
       </button>
 
-      <a className="navbar-brand" href="#">
-        {icon(faFolderMinus)}
-        {' '}
-        Meu Portifólio
+      <a className="navbar-brand" href="/">
+        {icon(faFolderMinus)} Meu Portifólio
       </a>
 
       <section className="collapse navbar-collapse" id="navbarMainToggler">
         <div className="navbar-nav">
-          <a className="nav-item nav-link" href="#skills">Skills</a>
-          <a className="nav-item nav-link" href="#formacao">Formação</a>
-          <a className="nav-item nav-link" href="#projetos">Projetos</a>
-          <a className="nav-item nav-link" href="#contato">Contato</a>
+          <a className="nav-item nav-link" href="#skills">
+            Skills
+          </a>
+          <a className="nav-item nav-link" href="#formacao">
+            Formação
+          </a>
+          <a className="nav-item nav-link" href="#projetos">
+            Projetos
+          </a>
+          <a className="nav-item nav-link" href="#contato">
+            Contato
+          </a>
         </div>
       </section>
-
     </nav>
   </header>
 );
