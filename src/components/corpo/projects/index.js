833cf17c1032f94b06316f8d0772926c4c53147c
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-16 14:44:02 -0300

    remove coments

--- src/components/corpo/projects/index.js
+++ src/components/corpo/projects/index.js
@@ -7,7 +7,7 @@ export default function Imagem() {
 
   useEffect(async () => {
     const response = await fetch(
-      "https://api.github.com/users/mateusfg7/repos",
+      "https://api.github.com/users/mateusfg7/repos"
     );
     const data = await response.json();
 
@@ -41,64 +41,3 @@ export default function Imagem() {
     </div>
   );
 }
-
-/*
-export default class Imagem extends Component {
-
-    loadInfo = async () => {
-        const response = await api.get('/users/mateusfg7/repos');
-        this.setState({ dados: response.data });
-
-        this.setState({ projeto1: this.state.dados[6]});
-        this.setState({ projeto2: this.state.dados[17]});
-        this.setState({ projeto3: this.state.dados[7]});
-        this.setState({ projeto4: this.state.dados[13]});
-        this.setState({ projeto5: this.state.dados[5]});
-    };
-
-    render() {
-        return (
-            <div className="main">
-
-                <h2 id="projetos">Projetos</h2>
-
-                <div className="project">
-                    <h3>{this.state.projeto1.name}</h3>
-                    <p>{this.state.projeto1.description}</p>
-                    <p>{this.state.projeto1.language}</p>
-                    <a href={this.state.projeto1.svn_url} target="_blank">GitHub</a>
-                </div>
-
-                <div className="project">
-                    <h3>{this.state.projeto2.name}</h3>
-                    <p>{this.state.projeto2.description}</p>
-                    <p>{this.state.projeto2.language}</p>
-                    <a href={this.state.projeto2.svn_url} target="_blank">GitHub</a>
-                </div>
-
-                <div className="project">
-                    <h3>{this.state.projeto3.name}</h3>
-                    <p>{this.state.projeto3.description}</p>
-                    <p>{this.state.projeto3.language}</p>
-                    <a href={this.state.projeto3.svn_url} target="_blank">GitHub</a>
-                </div>
-
-                <div className="project">
-                    <h3>{this.state.projeto4.name}</h3>
-                    <p>{this.state.projeto4.description}</p>
-                    <p>{this.state.projeto4.language}</p>
-                    <a href={this.state.projeto4.svn_url} target="_blank">GitHub</a>
-                </div>
-
-                <div className="project">
-                    <h3>{this.state.projeto5.name}</h3>
-                    <p>{this.state.projeto5.description}</p>
-                    <p>{this.state.projeto5.language}</p>
-                    <a href={this.state.projeto5.svn_url} target="_blank">GitHub</a>
-                </div>
-
-            </div>
-        );
-  }
-}
-*/
