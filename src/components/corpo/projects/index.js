e883d46ce2a331845d852df02e01330edd5aa3ef
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-05-04 17:02:05 +0000

    update projects rule

--- src/components/corpo/projects/index.js
+++ src/components/corpo/projects/index.js
@@ -3,32 +3,28 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 
 export default function Imagem() {
-  const [repositories, setRepositories] = useState([]);
-
-  useEffect(async () => {
-    const response = await fetch(
-      'https://api.github.com/users/mateusfg7/repos',
-    );
-    const data = await response.json();
-
-    if (response.status === 403) {
-      setRepositories(data);
-    } else if (response.status === 200) {
-      setRepositories([
-        data[10],
-        data[27],
-        data[13],
-        data[3],
-        data[9],
-        data[28],
-      ]);
+  const projects = [
+    {
+      name: 'Box-CSS-Preview',
+      description: 'A simple web page to see the preview of border css styles and get code',
+      repo: 'https://github.com/mateusfg7/Box-CSS-Preview',
+      technologies: ['JavaScript', 'ReactJS', 'CSS', 'Boostrap', 'HTML']
     }
-  }, []);
+  ]
+
 
   return (
     <div className="main">
       <h2 id="projetos">Projetos</h2>
-      {repositories.map((repo) => (
+      <div className="project">
+        <h3>Box-CSS-Preview</h3>
+        <p>A simple web page to see the preview of border css styles and get code</p>
+        <p>JavaScript</p>
+        <a href="https://github.com/mateusfg7/Box-CSS-Preview" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </div>
+      {/* repositories.map((repo) => (
         <div className="project">
           <h3>{repo.name}</h3>
           <p>{repo.description}</p>
@@ -37,7 +33,7 @@ export default function Imagem() {
             GitHub
           </a>
         </div>
-      ))}
+      )) */}
     </div>
   );
 }
