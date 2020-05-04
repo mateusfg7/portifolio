b22493427f5c179bc53044c781ba513900586e50
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-05-04 20:22:11 +0000

    update projects

--- src/components/corpo/projects/index.js
+++ src/components/corpo/projects/index.js
@@ -7,7 +7,7 @@ export default function Imagem() {
     {
       name: 'Box-CSS-Preview',
       description: 'A simple web page to see the preview of border css styles and get code',
-      repo: 'https://github.com/mateusfg7/Box-CSS-Preview',
+      url: 'https://github.com/mateusfg7/Box-CSS-Preview',
       technologies: ['JavaScript', 'ReactJS', 'CSS', 'Boostrap', 'HTML']
     }
   ]
@@ -16,24 +16,24 @@ export default function Imagem() {
   return (
     <div className="main">
       <h2 id="projetos">Projetos</h2>
-      <div className="project">
-        <h3>Box-CSS-Preview</h3>
-        <p>A simple web page to see the preview of border css styles and get code</p>
-        <p>JavaScript</p>
-        <a href="https://github.com/mateusfg7/Box-CSS-Preview" target="_blank" rel="noopener noreferrer">
-          GitHub
-        </a>
-      </div>
-      {/* repositories.map((repo) => (
-        <div className="project">
-          <h3>{repo.name}</h3>
-          <p>{repo.description}</p>
-          <p>{repo.language}</p>
-          <a href={repo.svn_url} target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-        </div>
-      )) */}
+      {
+        projects.map((repositoryInfo) => (
+          <div className="project">
+            <h3>{repositoryInfo.name}</h3>
+            <p>{repositoryInfo.description}</p>
+            <p>
+              {
+                repositoryInfo.technologies.map(technology => (
+                  <span className={technology}>{technology}</span>
+                ))
+              }
+            </p>
+            <a href={repositoryInfo.url} target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </div>
+        ))
+      }
     </div>
   );
 }
