9cab1bf0f0fa15e05b2b1035c1b6abfd5aed8144
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-05-05 18:51:28 +0000

    Updates

---
+++ precache-manifest.5eb21ba6ebb0a0d6b7c55083853c38ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90a33ceb61bb79559238c9d6017aee6",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "9f5bdcc8dc955cef9767",
+    "url": "/portifolio/static/css/main.e84ecc09.chunk.css"
+  },
+  {
+    "revision": "abd1e788cb5a2e5d16a4",
+    "url": "/portifolio/static/js/2.958f958b.chunk.js"
+  },
+  {
+    "revision": "9f5bdcc8dc955cef9767",
+    "url": "/portifolio/static/js/main.34028f0e.chunk.js"
+  },
+  {
+    "revision": "8ef60b4e15b94ce10768",
+    "url": "/portifolio/static/js/runtime-main.38f728ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portifolio/precache-manifest.281b14f0281bb79f910e3408ed84f87f.js"
+  "/portifolio/precache-manifest.5eb21ba6ebb0a0d6b7c55083853c38ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portifolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
