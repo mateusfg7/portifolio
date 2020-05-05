05af3d3167a399f869dbc0ad3ef81c9a5a914b14
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-05-05 16:14:23 +0000

    update projects

--- src/components/corpo/projects/index.js
+++ src/components/corpo/projects/index.js
@@ -5,35 +5,28 @@ import './style.css';
 import './technologies.css';
 
 export default function Imagem() {
-  const projects = [
-    {
-      name: 'Box-CSS-Preview',
-      description: 'Uma ferramenta feita com React JS para obter um preview em tempo real de bordas de um componente HTML e obter o código CSS respectivo a estilização escolhida.',
-      url: 'https://github.com/mateusfg7/Box-CSS-Preview',
-    },
-    {
-      name: 'textAnalysis',
-      description: 'Programa feito em Python3 que realiza análise de texto usando recursos da API Algorithmia, como resumir textos, obter tags de acordo com o assunto do texto, obter frequencia de palavras, êtc.',
-      url: 'https://github.com/mateusfg7/textAnalysis',
-      technologies: ['Python3,', 'Algorithmia,', 'GoogleTrans'],
-    },
-  ];
-
-
   return (
     <div className="main">
-      <h2 id="projetos">Projetos</h2>
-      {
-        projects.map((repositoryInfo) => (
-          <div className="project">
-            <h3>{repositoryInfo.name}</h3>
-            <p>{repositoryInfo.description}</p>
-            <a href={repositoryInfo.url} target="_blank" rel="noopener noreferrer">
-              Código Fonte
-            </a>
-          </div>
-        ))
-      }
+      <h2 id="projetos">Principais projetos</h2>
+
+      <div className="project">
+        <h3>Box-CSS-Preview</h3>
+        <p>Uma ferramenta feita com React JS para obter um preview em tempo real de bordas de um componente HTML e obter o código CSS respectivo a estilização escolhida.</p>
+        <a href="https://mateusfg7.github.io/Box-CSS-Preview/" target="_blank" rel="noopener noreferrer" className="web-page-button">
+          WebPage
+        </a>
+        <a href="https://github.com/mateusfg7/Box-CSS-Preview" target="_blank" rel="noopener noreferrer" className="source-code-button">
+          Código Fonte
+        </a>
+      </div>
+
+      <div className="project">
+        <h3>textAnalysis</h3>
+        <p>Programa feito em Python3 que realiza análise de texto usando recursos da API Algorithmia, como resumir textos, obter tags de acordo com o assunto do texto, obter frequencia de palavras, êtc.</p>
+        <a href="https://github.com/mateusfg7/textAnalysis" target="_blank" rel="noopener noreferrer" className="source-code-button">
+          Código Fonte
+        </a>
+      </div>
     </div>
   );
 }
