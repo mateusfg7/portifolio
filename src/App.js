66f1fe3a53b7b4c317dfb68f584ae4d7a8e70ebc
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-05-06 10:44:23 +0000

    RESET_TEMPLATE

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React from "react";
+import React from 'react';
 /* import './reset.css' */
-import "./App.css";
-import "./variables.css";
+import './App.css';
+import './variables.css';
 
-import Header from "./components/header";
-import Corpo from "./components/corpo";
-import Footer from "./components/footer";
 
 export default function App() {
   return (
     <div className="container-fluid p-0">
-      <Header />
-      <Corpo />
-      <Footer />
+      Test
     </div>
   );
 }
