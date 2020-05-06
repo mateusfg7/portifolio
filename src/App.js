09d537b1a4224fe45a5e488ddfce553527a19167
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2020-05-06 11:49:21 +0000

    init home construction

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ import './variables.css';
 export default function App() {
   return (
     <div>
-      Test
+      <section className="home">
+        <div className="about">
+          <div className="description">
+            Meu nome é Mateus Felipe, tenho 17 anos,
+            gosto de sempre estar estudando e aprendendo coisas novas,
+            principalmente no ramo de tecnologia.
+          </div>
+          <div className="profile-photo">
+            {/* <img src="https://i.imgur.com/EFeEbuJ.jpg" alt="" /> */}
+            <img src="https://i.imgur.com/Ai49JH1.gif" alt="" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
