530608029e0d5d028a633f8f7f29769f4c15d88f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-09 17:49:05 -0300

    creating and usign routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React from 'react';
-import './reset.css';
-import './App.css';
-import './variables.css';
+import React from "react";
 
+import Routes from "./Routes";
+
+import "./reset.css";
+import "./App.css";
+import "./variables.css";
 
 export default function App() {
-  return (
-    <div>
-      <section className="home">
-        <div className="about">
-          <div className="description">
-            Meu nome é Mateus Felipe, tenho 17 anos,
-            gosto de sempre estar estudando e aprendendo coisas novas,
-            principalmente no ramo de tecnologia.
-          </div>
-          <div className="profile-photo">
-            {/* <img src="https://i.imgur.com/EFeEbuJ.jpg" alt="" /> */}
-            <img src="https://i.imgur.com/Ai49JH1.gif" alt="" />
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+  return <Routes />;
 }
