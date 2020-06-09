1adad84904bc2b25902b2229ea7ab26e9c72a379
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-09 17:49:40 -0300

    Home page creating

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <section className="home">
+        <div className="about">
+          <div className="description">
+            Meu nome é Mateus Felipe, tenho 17 anos, gosto de sempre estar
+            estudando e aprendendo coisas novas, principalmente no ramo de
+            tecnologia.
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
