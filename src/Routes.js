f9c754f595c2763ab6153844c7f80ddccdc9847f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-10 11:55:41 -0300

    adding new pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-// import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Switch>{/* <Route path="/" exact component={Home} /> */}</Switch>
+      <Switch>
+        <Route path="/" exact component={About} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ src/pages/Projects/index.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+
+export default function Projects() {
+  useEffect(() => {
+    const projectsButton = document.getElementById("projects-button");
+    projectsButton.classList.add("selected");
+  }, []);
+
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit
+        fringilla nisi sed tempor. In lacinia auctor iaculis. Sed vel porta
+        metus. In ut leo nunc. Vivamus et sollicitudin sem, at tristique quam.
+        Proin vitae ante nec tellus mattis dictum sit amet vel erat. In hac
+        habitasse platea dictumst. Pellentesque dictum mauris eget luctus
+        vehicula. Vivamus tincidunt nunc rhoncus, fringilla nisi vitae, finibus
+        nibh. Mauris non libero ut felis rutrum dapibus. Fusce egestas aliquet
+        leo, sit amet laoreet tortor euismod sed.
+      </p>
+      <p>
+        Sed a felis id diam elementum ultricies. Suspendisse est turpis,
+        placerat non accumsan convallis, imperdiet eget felis. Suspendisse
+        cursus augue magna, vitae finibus eros pretium ac. Sed justo enim,
+        vulputate ut egestas nec, aliquet sit amet felis. Mauris lacus diam,
+        fermentum vel eros quis, laoreet suscipit purus. Sed pharetra ultrices
+        massa. Sed id porta sapien. Aenean blandit rhoncus rhoncus.
+      </p>
+      <p>
+        Aliquam velit leo, imperdiet at magna eu, pretium vulputate odio.
+        Phasellus mi purus, ultricies in dictum non, elementum eu tortor.
+        Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent
+        per conubia nostra, per inceptos himenaeos. Sed vitae eros rhoncus quam
+        iaculis volutpat ut vitae lorem. Pellentesque non rutrum augue.
+        Vestibulum volutpat sapien a sem fermentum pellentesque. Sed sodales
+        vitae.
+      </p>
+    </div>
+  );
+}
