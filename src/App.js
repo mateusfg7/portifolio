a2a530268ebc55c7a3a8e5e14be00ce4b5b61e8c
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-10 09:49:53 -0300

    wave-effect addition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+import Home from "./pages/Home";
 import Routes from "./Routes";
 
 import "./App.css";
+import "./styles/wave-effect.css";
 
 export default function App() {
-  return <Routes />;
+  return (
+    <div>
+      <Home />
+      <section className="wave-effect">
+        <div className="wave wave1" />
+        <div className="wave wave2" />
+        <div className="wave wave3" />
+        <div className="wave wave4" />
+      </section>
+      <main id="Main">
+        <Routes />
+      </main>
+    </div>
+  );
 }
