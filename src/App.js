816c4de1d1205477dd665fec903d0a7eb9f172b2
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-10 09:55:01 -0300

    mv 'Home' from pages to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Home from "./pages/Home";
+import Home from "./components/Home";
 import Routes from "./Routes";
 
 import "./App.css";
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Switch>{/* <Route path="/" exact component={Home} /> */}</Switch>
     </BrowserRouter>
   );
 }
