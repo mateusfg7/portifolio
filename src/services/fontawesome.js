9de70818c1323d329192783e24d9f51ad6cebcbc
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-11 09:56:24 -0300

    fontawesome.js delete

--- src/services/fontawesome.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-const icon = (iconName) => <FontAwesomeIcon icon={iconName} />;
-
-export default icon;
