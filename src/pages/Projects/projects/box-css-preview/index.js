295647253f8cfb8836f7c36d528f42373465dd29
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-11 11:14:27 -0300

    add new projects pages

---
+++ src/pages/Projects/projects/box-css-preview/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BoxCssPreview() {
+  return <h1 id="box-css-preview">Box-CSS-Preview</h1>;
+}
---
+++ src/pages/Projects/projects/ecoleta/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Ecoleta() {
+  return <h1 id="ecoleta">Ecoleta</h1>;
+}
