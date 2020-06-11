85bcf0f86909de9b7cd7e9f658b26b2d30835621
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-11 11:12:57 -0300

    add new routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 
+import BoxCssPreview from "./pages/Projects/projects/box-css-preview";
+import Ecoleta from "./pages/Projects/projects/ecoleta";
+
 export default function Routes() {
   return (
     <BrowserRouter>
@@ -11,6 +14,8 @@ export default function Routes() {
         <Route exact path="/" component={About} />
         <Route path="/about" component={About} />
         <Route exact path="/projects" component={Projects} />
+        <Route path="/projects/box-css-preview" component={BoxCssPreview} />
+        <Route path="/projects/ecoleta" component={Ecoleta} />
       </Switch>
     </BrowserRouter>
   );
