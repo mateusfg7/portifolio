838452288a1bd48d1c2eeda1797415520d7fc554
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-05-17 11:09:48 -0300

    small changes

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 
+import axios from "axios";
+
 import Menu from "../../components/Menu";
 
 import "./styles.css";
@@ -8,7 +10,7 @@ export default function About() {
   useEffect(() => {
     const aboutButton = document.getElementById("about-button");
     aboutButton.classList.add("selected");
-  }, []);
+  });
 
   return (
     <div>
@@ -16,14 +18,14 @@ export default function About() {
       <h2 id="about">About</h2>
       <div className="description">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit
-          fringilla nisi sed tempor. In lacinia auctor iaculis. Sed vel porta
-          metus. In ut leo nunc. Vivamus et sollicitudin sem, at tristique quam.
-          Proin vitae ante nec tellus mattis dictum sit amet vel erat. In hac
-          habitasse platea dictumst. Pellentesque dictum mauris eget luctus
-          vehicula. Vivamus tincidunt nunc rhoncus, fringilla nisi vitae, finibus
-          nibh. Mauris non libero ut felis rutrum dapibus. Fusce egestas aliquet
-          leo, sit amet laoreet tortor euismod sed.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          hendrerit fringilla nisi sed tempor. In lacinia auctor iaculis. Sed
+          vel porta metus. In ut leo nunc. Vivamus et sollicitudin sem, at
+          tristique quam. Proin vitae ante nec tellus mattis dictum sit amet vel
+          erat. In hac habitasse platea dictumst. Pellentesque dictum mauris
+          eget luctus vehicula. Vivamus tincidunt nunc rhoncus, fringilla nisi
+          vitae, finibus nibh. Mauris non libero ut felis rutrum dapibus. Fusce
+          egestas aliquet leo, sit amet laoreet tortor euismod sed.
         </p>
         <p>
           Sed a felis id diam elementum ultricies. Suspendisse est turpis,
@@ -37,8 +39,8 @@ export default function About() {
           Aliquam velit leo, imperdiet at magna eu, pretium vulputate odio.
           Phasellus mi purus, ultricies in dictum non, elementum eu tortor.
           Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent
-          per conubia nostra, per inceptos himenaeos. Sed vitae eros rhoncus quam
-          iaculis volutpat ut vitae lorem. Pellentesque non rutrum augue.
+          per conubia nostra, per inceptos himenaeos. Sed vitae eros rhoncus
+          quam iaculis volutpat ut vitae lorem. Pellentesque non rutrum augue.
           Vestibulum volutpat sapien a sem fermentum pellentesque. Sed sodales
           vitae.
         </p>
